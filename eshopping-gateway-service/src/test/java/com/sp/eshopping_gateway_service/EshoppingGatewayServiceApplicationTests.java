a01283d1f3b0fdac154d5f68025180536ae1468b
suraj-shelke <shelkesuraj205@gmail.com>
2025-11-18 12:59:40 +0530

    gateway service

---
+++ eshopping-gateway-service/src/test/java/com/sp/eshopping_gateway_service/EshoppingGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.eshopping_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
